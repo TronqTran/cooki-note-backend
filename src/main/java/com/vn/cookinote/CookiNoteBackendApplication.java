7e076b7c6a15fb692ccfbc9d76857830829ec16b
TRONGTB9 <trongtb9@fpt.com>
2025-12-10 23:01:42 +0700

    Update UserServiceImpl: change error message for email not found to Vietnamese

--- src/main/java/com/vn/cookinote/CookiNoteBackendApplication.java
+++ src/main/java/com/vn/cookinote/CookiNoteBackendApplication.java
@@ -1,21 +1,7 @@
 package com.vn.cookinote;
 
-import com.vn.cookinote.enums.MediaType;
-import com.vn.cookinote.enums.Role;
-import com.vn.cookinote.enums.Status;
-import com.vn.cookinote.models.Media;
-import com.vn.cookinote.models.User;
-import com.vn.cookinote.repositories.MediaRepository;
-import com.vn.cookinote.repositories.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import jakarta.transaction.Transactional;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class CookiNoteBackendApplication {
